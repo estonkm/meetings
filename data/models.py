fe814bddc7f238010bfb706cf8aff967d69df22c
splichte <splichte@princeton.edu>
2013-07-12 10:09:25 -0400

    from pil import image

--- data/models.py
+++ data/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-#import PIL
-import Image
+from PIL import Image
 
 class Account(models.Model):
 	user = models.OneToOneField(User)
